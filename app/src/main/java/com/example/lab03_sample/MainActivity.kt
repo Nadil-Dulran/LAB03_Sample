5e447895a08ccf96ac288ae6760432e3b558bcd6
Nadil-Dulran <it23161788@my.sliit.lk>
2026-03-01 17:25:34 +0530

    Update app logics

--- app/src/main/java/com/example/lab03_sample/MainActivity.kt
+++ app/src/main/java/com/example/lab03_sample/MainActivity.kt
@@ -1,20 +1,78 @@
 package com.example.lab03_sample
 
+import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.lab03_sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val edtName: EditText = binding.edtName
+        val edtEmail: EditText = binding.edtEmail
+        val edtPhone: EditText = binding.edtPhone
+        val edtPassword: EditText = binding.edtPassword
+        val edtRePassword: EditText = binding.edtRePassword
+        val btnSubmit: Button = binding.btnSubmit
+        val btnCancel: Button = binding.btnCancel
+
+
+    fun showAlertBox(
+        context: Context,
+        name: String,
+        email: String,
+        phone: String,
+        password: String,
+        rePassword: String
+    ) {
+
+        val builder = AlertDialog.Builder(context)
+
+        val message = "Email: $email\n" +
+                "Phone: $phone\n" +
+                "Passwords: ${if (password == rePassword) "Matching" else "Not Matching"}."
+
+        builder.setTitle("Welcome $name!")
+        builder.setMessage(message)
+
+        builder.setPositiveButton("Ok") { _, _ ->
+            Toast.makeText(context, "Submitted", Toast.LENGTH_LONG).show()
+        }
+
+        builder.setNegativeButton("Cancel") { _, _ -> }
+
+        val dialog = builder.create()
+        dialog.show()
+    }
+        btnSubmit.setOnClickListener {
+            showAlertBox(
+                this,
+                edtName.text.toString(),
+                edtEmail.text.toString(),
+                edtPhone.text.toString(),
+                edtPassword.text.toString(),
+                edtRePassword.text.toString()
+            )
+        }
+
+        btnCancel.setOnClickListener {
+            edtName.setText("")
+            edtEmail.setText("")
+            edtPhone.setText("")
+            edtPassword.setText("")
+            edtRePassword.setText("")
         }
     }
+
 }
