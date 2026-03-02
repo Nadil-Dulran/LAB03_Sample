6f9529956be6aa42eb5b9c590b0916f9706af541
Nadil-Dulran <it23161788@my.sliit.lk>
2026-03-02 11:40:28 +0530

    Create class data

---
+++ app/src/main/java/com/example/lab03_sample/FormData.kt
@@ -0,0 +1,9 @@
+package com.example.lab03_sample
+
+data class FormData(
+    val name: String,
+    val email: String,
+    val phone: String,
+    val password: String,
+    val rePassword: String
+)
